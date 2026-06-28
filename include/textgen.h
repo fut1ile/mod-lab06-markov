25ebe1800fb753e84b1a7175d7688cea118be87a
fut1ile <158841721+fut1ile@users.noreply.github.com>
2026-06-28 16:23:26 +0300

    Create textgen.h

---
+++ include/textgen.h
@@ -0,0 +1,33 @@
+#ifndef INCLUDE_TEXTGEN_H_
+#define INCLUDE_TEXTGEN_H_
+
+#include <deque>
+#include <map>
+#include <random>
+#include <string>
+#include <vector>
+
+class MarkovTextGenerator {
+ private:
+  typedef std::deque<std::string> Prefix;
+  typedef std::map<Prefix, std::vector<std::string> > StateTable;
+
+  StateTable statetab;
+  int npref;
+  std::mt19937 rng;
+
+ public:
+  explicit MarkovTextGenerator(int prefixSize = 2);
+
+  void buildFromFile(const std::string& filename);
+  void buildFromText(const std::string& text);
+
+  std::string generate(int maxWords);
+
+  StateTable& getStateTable();
+  void clear();
+  void addSuffix(const Prefix& prefix, const std::string& suffix);
+  std::string getRandomSuffix(const Prefix& prefix);
+};
+
+#endif
